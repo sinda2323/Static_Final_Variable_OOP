3c9773620fb821d65a788c837c3484d7336b8edf
sinda2323 <sindadwilia9@gmail.com>
2026-04-22 21:18:01 +0700

    after

--- tugas_1.php
+++ tugas_1.php
@@ -8,12 +8,21 @@ class Pengunjung {
         self::$jumlah++;
     }
 
+    public static function reset() {
+        self::$jumlah = 0;
+    }
 }
 
 $p1 = new Pengunjung();
 $p2 = new Pengunjung();
 $p3 = new Pengunjung();
+$p4 = new Pengunjung();
+$p5 = new Pengunjung();
 
-echo "Jumlah Pengunjung: " . Pengunjung::$jumlah;
+echo "Jumlah Pengunjung: " . Pengunjung::$jumlah . "<br>";
+
+// Contoh penggunaan method reset
+Pengunjung::reset();
+echo "Setelah reset: " . Pengunjung::$jumlah;
 
 ?>
